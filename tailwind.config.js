af7437ca5fd0081d31231d1c1ff4f90bad1715b4
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-12-17 12:43:56 +0900

    以下のファイルに出力出来るように、content配列を修正
    - "./resources/**/*.blade.php"
    - "./resources/**/*.vue"
    - "./resources/**/*.js"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.vue",
+    "./resources/**/*.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
