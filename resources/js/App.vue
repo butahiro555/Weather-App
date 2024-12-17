9920fc6532fe6d6914f7cab770fa03f78307adda
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-12-17 12:41:16 +0900

    お天気アプリのためのApp.vueファイルを作成

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar />
+  <div class="min-h-screen bg-neutral-800">
+    <!-- タブコンテナを表示 -->
+    <TabContainer />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import TabContainer from './components/TabContainer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    TabContainer,
+  },
+};
+</script>
