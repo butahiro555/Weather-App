ea56efff9732668b0449195b1c4deabb6af0626c
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-12-18 18:30:22 +0900

    TabContainer.vueからAPIリクエストをGetWeatherControllerに送信して、天気情報のURLを要求するAPIルートを作成しました

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GetWeatherController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+// TabContainer.vueからAPIリクエストをGetWeatherControllerに送信して、天気情報のURLを要求するAPIルート
+Route::get('/getWeather', [GetWeatherController::class, 'getCoordinates'])->name('getWeather');
